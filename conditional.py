cb7025b3fdea3d618f709038452e301d2e2fee4b
Joe Tabor <jatabor@u.arizona.edu>
2016-01-30 16:26:34 -0700

    inital commit

---
+++ conditional.py
@@ -0,0 +1,6 @@
+x = 7
+
+if x < 10:
+    print("That make sense")
+else:
+    print("I don't think this will happen")
---
+++ iteration.py
@@ -0,0 +1,11 @@
+books = ["Tale of two cities", "Harry Potter", "Lord of the Rings"]
+nums = range(100)
+mix = [books, list(nums)]
+
+ 
+for book in books:
+    print (book)
+for num in nums:
+    print (num)
+for it in mix:
+    print (it)
